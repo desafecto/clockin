fd3b531250c132ea1e93d096d642d1d890d2e967
desafecto <eldesafecto@gmail.com>
2020-07-05 21:39:34 +0200

    gitignore file and so on

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,5 @@
-
-COMPANY_UST = 'Your company code'
-CHROME_DRIVER = 'Your webdriver local path'
-URL = f"https://a3gt.wolterskluwer.es/gt#/clockings/{COMPANY_UST}"
-USER_ID = 'your own one'
-PASS = 'your own one'
+COMPANY_CODE = '12345'                                               # Put your company id as a string
+CHROME_DRIVER = '<LOCAL PATH>'                                       # Put your local path of driver you have
+URL = f"https://a3gt.wolterskluwer.es/gt#/clockings/{COMPANY_CODE}"
+USER_ID = 'xyz'                                                      # Put your own employe identifier as a string
+PASS = '123'                                                         # The required password you must know. Set it as a string
