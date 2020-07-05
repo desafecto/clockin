ca6efd59d6e152d4b545907ae16c48468806a0ad
desafecto <eldesafecto@gmail.com>
2020-07-05 21:50:56 +0200

    minor changes

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
-COMPANY_CODE = '16457'                                               # Put your company id as a string
+COMPANY_CODE = '12345'                                               # Put your company id as a string
 CHROME_DRIVER = 'D:\\chromedriver\\chromedriver.exe'                 # Put your local path of driver you have
 URL = f"https://a3gt.wolterskluwer.es/gt#/clockings/{COMPANY_CODE}"
-USER_ID = '00816236N'                                                # Put your own employe identifier as a string
-PASS = '012'                                                         # The required password you must know. Set it as a string
+USER_ID = '123456789'                                                # Put your own employe identifier as a string
+PASS = '123'                                                         # The required password you must know. Set it as a string
